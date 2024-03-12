f2d2b1a83a641fcb1ab52f1ee10e076296a77115
enjxz97382 <enjxz97382@gmail.com>
2024-03-12 07:12:33 +0000

    directory 구조체 생성

--- src/include/file.h
+++ src/include/file.h
@@ -5,6 +5,7 @@
 #include "device.h"
 
 #define BLOCK_SIZE 4096
+#define MAX_FILENAME_LENGTH 256
 
 /**
  * fnode - data structure of file metadata
@@ -21,6 +22,29 @@ typedef struct fnode {
     off_t size;
     unsigned int* lba_array;
 } fnode;
+
+/**
+ * DirectoryEntry - data structure of directory mapping table
+ * @filename: file name of directory
+ * @fnode_number: index of fnode
+*/
+typedef struct DirectoryEntry {
+    char fileName[MAX_FILENAME_LENGTH];
+    unsigned int inodeNumber;
+} DirectoryEntry;
+
+/**
+ * Directory - data structure of directory
+ * @entries: entries of directory
+ * @size: number of directory
+ * @capacity: memory size for saving directory entries 
+*/
+typedef struct Directory {
+    DirectoryEntry* entries;
+    size_t size;
+    size_t capacity;
+} Directory;
+
 
 void gen_append_args(struct nvme_zns_append_args*, __u64 zslba, void *data, ZoneDeviceInfo device);
 
