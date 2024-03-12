1fbcd212185d64a15a64e91c61df9c6413f1a8da
enjxz97382 <enjxz97382@gmail.com>
2024-03-12 02:19:03 +0000

    fnode 구조체 생성

--- src/include/file.h
+++ src/include/file.h
@@ -6,11 +6,20 @@
 
 #define BLOCK_SIZE 4096
 
+/**
+ * fnode - data structure of file metadata
+ * @fnode_number: number of fnode
+ * @file_type: 'f' for file, 'd' for directory
+ * @permission: File permissions
+ * @size: Size of the file in bytes
+ * @lba_array: data lba of the file
+*/
 typedef struct fnode {
-    mode_t mode;
-    unsigned int fd;
-    unsigned int* zslba_array;
+    unsigned int fnode_number;
+    char file_type;
+    mode_t permission;
     off_t size;
+    unsigned int* lba_array;
 } fnode;
 
 void gen_append_args(struct nvme_zns_append_args*, __u64 zslba, void *data, ZoneDeviceInfo device);
